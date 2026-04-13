2891f397f59dc6e0e473dc5af608456b82bdad2c
kennypavelka <kenny@pavelka.com>
2026-04-13 16:11:16 -0300

    Inicio exercicio 2 - Adicionado metodo de depositar e sacar

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -14,4 +14,37 @@ public class ContaBancaria {
         System.out.println("Contra criada com sucesso! Bonius de R$ 50.00 aplicado! ");
 
     }
+
+    void depositar(double valor){
+        if (valor >0) {
+            saldo += valor;
+
+            System.out.printf("Ddeposito de R$ %.2f realizado! Saldo atual: #R %.2f%n", valor, saldo);
+        } else {
+            System.out.println(" Valor invalido para deposito." );
+
+        }
+    }
+
+    void sacaar(double valor) {
+        if (quantidadeSaques >=3) {
+
+            System.out.println(" Limite de saques diarios atingisdos.");
+        } else if (valor > 1000.00) {
+            System.out.println("Valor maximo por saque é R$ 1.000,00.");
+        }else if (valor > saldo) {
+            System.out.println("SALDO insifuciente. ");
+        }else if (valor <= 0){
+            System.out.println("Valor invalido.");
+        }else {
+
+            saldo -= valor;
+            quantidadeSaques++;
+
+            System.out.printf("Saque de R$ %.2f realizado! Saldo restante: R# %.2fn", valor, saldo);
+            System.out.println("Saques rezliados hoje: " + quantidadeSaques +"/3");
+
+
+        }
+    }
 }
