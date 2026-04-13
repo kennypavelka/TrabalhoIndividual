bdfa38db7d57820912017466bedf4cc177145fb5
kennypavelka <kenny@pavelka.com>
2026-04-13 16:03:13 -0300

    Inicio exercicio 2 - Criação da Conta Bancaria Adicionados atributos e construtorr

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -1,3 +1,17 @@
 public class ContaBancaria {
 
+
+    int numero;
+    String titular;
+    double saldo;
+    int quantidadeSaques;
+
+    ContaBancaria(int numero, String titular) {
+        this.numero = numero;
+        this.titular = titular;
+        this.saldo = 50.00;    //saldo começando em 50 pelo bonus
+        this.quantidadeSaques = 0;
+        System.out.println("Contra criada com sucesso! Bonius de R$ 50.00 aplicado! ");
+
+    }
 }
