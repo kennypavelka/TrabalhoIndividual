f255beec261f3454bfb35245442e44afe8c38329
kennypavelka <kenny@pavelka.com>
2026-04-18 18:30:46 -0300

    Inicio exercicio 3
    finalizanod main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Main{
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Cliente cliente1 = new Cliente("João Silva", "123.456.789-00");
+
+        Pedido pedido1 = new Pedido(1001, cliente1);
+        pedido1.adicoinarItem(new ItemPedido("Teclado mecanido", 170.00, 1));
+        pedido1.adicoinarItem(new ItemPedido("Mouse Gamer", 80.00, 1);
+        pedido1.fecharPedido();
+
+        System.out.println();
+
+        Cliente cliente2 = new Cliente("Pavelka ok", "195.231.222-22");
+        Pedido pedido2 = new Pedido(1002, cliente2);
+
+        pedido2.adicoinarItem(new ItemPedido("Monitor 24 polegadas", 900.00, 1));
+        pedido2.adicoinarItem(new ItemPedido("Cadeira Gamer", 1200);
+        pedido2.fecharPedido();
+
+
     }
 }
