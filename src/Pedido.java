d69f2002f4042841d2f42b3a3dd17188e9a48e42
kennypavelka <kenny@pavelka.com>
2026-04-18 18:02:39 -0300

    Inicio exercicio 3
    fazendo a parte de imprimir o recebioid

--- src/Pedido.java
+++ src/Pedido.java
@@ -33,5 +33,44 @@ public double calcularTotalItens() {
         double totalItens = calcularTotalItens()
 
 
+                if (totaItens > 250.00) {
+
+                    frete = 0:
+                } else {
+                    frete = 25.00;
+
+                }
+
+                totalFinal = totalItens + frete;
+
+
+                System.out.println("---- recbo do pedido ----");
+     System.out.println("Pedido n: " +numeroPedido);
+     System.out.println("Cliente: " + cliente.nome + " CPF : " + cliente.cpf);
+     System.out.println(" =====--=======");
+     System.out.println("Itemns: ");
+
+     for (ItemPedido item : itens) {
+         System.out.printlf(" %-20s x%d  R#$ %.2f%n", item.nomeProduto, item.quantidade, item.getSubtotal());
+
+
+     }
+
+     System.out.println(" -------------------");
+     System.out.printlf("Subtotal: R# %.2f%n", totalItens);
+
+     if (frete ==0) {
+         System.out.println("Frete:  Gratis");
+     } else  {
+         System.out.printf("Frete: R$ %.2f%n", frete);
+
+     }
+
+     System.out.printlf("Total :    R$ %.2f%n", totalFinal);
+
  }
+
+ System.out.printlf("TOLtal:   R$ %.2f%n", totalFinal);
+    System.out.println(" -----------");
+
 }
